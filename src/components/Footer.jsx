758d967f287960bde257e0f17c9986b324e29cb7
turnertop <turnerfool@pm.me>
2023-04-28 17:37:55 -0700

    it's good enough

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,17 @@ import fbIcon from '../assets/icon-facebook.svg';
 import igIcon from '../assets/icon-instagram.svg';
 import liIcon from '../assets/icon-linkedin.svg';
 
-
-
 export default function Footer() {
   return (
     <footer id="footer" className="bg-black py-4 mt-20">
-        <div className="container mx-auto flex justify-between items-center">
-        <div className="text-sm text-white">© 2023 Boxchampy. All rights reserved.</div>
+      <div className="container mx-auto flex justify-between items-center flex-col md:flex-row">
+        <div className="text-xl text-white uppercase font-bold mb-4 md:mb-0">Ready for your next lesson?</div>
+        <div className="text-sm text-white mb-4 md:mb-0">© 2023 Boxchampy. All rights reserved.</div>
         <div>
           <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded">Sign up</button>
         </div>
       </div>
     </footer>
-
-
   );
 };
 
